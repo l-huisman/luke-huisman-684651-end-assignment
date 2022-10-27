fad7076fc6ba9711b02071d2aaa487f2c897f02c
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-10-27 11:46:58 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lukehuisman684651endassignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lukehuisman684651endassignment to javafx.fxml;
+    exports com.example.lukehuisman684651endassignment;
+}
